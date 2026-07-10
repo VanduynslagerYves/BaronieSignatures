a5c7710c6ecb299661ee4e2586034cf24d58c000
Yves Vanduynslager <yves.vanduyslager@baronie.com>
2026-07-10 09:28:52 +0200

    fix for email based signature

--- BaronieSignatures/SignatureParamsList.cs
+++ BaronieSignatures/SignatureParamsList.cs
@@ -26,70 +26,70 @@ public static class SignatureParamsList
             BaseLocal = Path.Combine(AppContext.BaseDirectory, "Output", "Brugge"),
             DefaultPhone = "+32 50 45 00 20",
         },
-        new SignatureParams
-        {
-            GroupName = "SG_Lokeren_DESKTOP_OUTLOOK_SIGNATURE_DEFAULT",
-            Company = "Baronie Lokeren",
-            SigSource = Path.Combine(AppContext.BaseDirectory, "Templates", "Lokeren"),
-            BaseLocal = Path.Combine(AppContext.BaseDirectory, "Output", "Lokeren"),
-            DefaultPhone = "+32 93 26 82 70",
-        },
-        new SignatureParams
-        {
-            GroupName = "SG_Veurne_DESKTOP_OUTLOOK_SIGNATURE_DEFAULT",
-            Company = "Baronie Veurne",
-            SigSource = Path.Combine(AppContext.BaseDirectory, "Templates", "Veurne"),
-            BaseLocal = Path.Combine(AppContext.BaseDirectory, "Output", "Veurne"),
-            DefaultPhone = "+32 58 31 01 50",
-        },
-        new SignatureParams
-        {
-            GroupName = "SG_CASLANO_DESKTOP_OUTLOOK_SIGNATURE_ALPROSE",
-            Company = "Alprose",
-            SigSource = Path.Combine(AppContext.BaseDirectory, "Templates", "Alprose"),
-            BaseLocal = Path.Combine(AppContext.BaseDirectory, "Output", "Alprose"),
-            DefaultPhone = "+41 91 611 88 88",
-        },
-        new SignatureParams
-        {
-            GroupName = "SG_CASLANO_DESKTOP_OUTLOOK_SIGNATURE_BARONIE_SWITZERLAND",
-            Company = "Baronie Switzerland",
-            SigSource = Path.Combine(AppContext.BaseDirectory, "Templates", "Switzerland"),
-            BaseLocal = Path.Combine(AppContext.BaseDirectory, "Output", "Switzerland"),
-            DefaultPhone = "+41 91 611 88 88",
-        },
-        new SignatureParams
-        {
-            GroupName = "SG_Norderstedt_DESKTOP_OUTLOOK_SIGNATURE_DEFAULT",
-            Company = "Baronie Norderstedt",
-            SigSource = Path.Combine(AppContext.BaseDirectory, "Templates", "Norderstedt"),
-            BaseLocal = Path.Combine(AppContext.BaseDirectory, "Output", "Norderstedt"),
-            DefaultPhone = "+49 40 52 60 20"
-        },
-        new SignatureParams
-        {
-            GroupName = "SG_Eupen_DESKTOP_OUTLOOK_SIGNATURE_DEFAULT",
-            Company = "Baronie Eupen",
-            SigSource = Path.Combine(AppContext.BaseDirectory, "Templates", "Eupen"),
-            BaseLocal = Path.Combine(AppContext.BaseDirectory, "Output", "Eupen"),
-            DefaultPhone = "+32 50 45 00 30"
-        },
-        new SignatureParams
-        {
-            GroupName = "SG_Saalfeld_DESKTOP_OUTLOOK_SIGNATURE_DEFAULT",
-            Company = "Baronie Saalfeld",
-            SigSource = Path.Combine(AppContext.BaseDirectory, "Templates", "Saalfeld"),
-            BaseLocal = Path.Combine(AppContext.BaseDirectory, "Output", "Saalfeld"),
-            DefaultPhone = "+49 40 526020"
-        },
-        new SignatureParams
-        {
-            GroupName = "SG_Berlin_DESKTOP_OUTLOOK_SIGNATURE_DEFAULT",
-            Company = "Baronie Berlin",
-            SigSource = Path.Combine(AppContext.BaseDirectory, "Templates", "Berlin"),
-            BaseLocal = Path.Combine(AppContext.BaseDirectory, "Output", "Berlin"),
-            DefaultPhone = "+49 40 526020"
-        },
+        //new SignatureParams
+        //{
+        //    GroupName = "SG_Lokeren_DESKTOP_OUTLOOK_SIGNATURE_DEFAULT",
+        //    Company = "Baronie Lokeren",
+        //    SigSource = Path.Combine(AppContext.BaseDirectory, "Templates", "Lokeren"),
+        //    BaseLocal = Path.Combine(AppContext.BaseDirectory, "Output", "Lokeren"),
+        //    DefaultPhone = "+32 93 26 82 70",
+        //},
+        //new SignatureParams
+        //{
+        //    GroupName = "SG_Veurne_DESKTOP_OUTLOOK_SIGNATURE_DEFAULT",
+        //    Company = "Baronie Veurne",
+        //    SigSource = Path.Combine(AppContext.BaseDirectory, "Templates", "Veurne"),
+        //    BaseLocal = Path.Combine(AppContext.BaseDirectory, "Output", "Veurne"),
+        //    DefaultPhone = "+32 58 31 01 50",
+        //},
+        //new SignatureParams
+        //{
+        //    GroupName = "SG_CASLANO_DESKTOP_OUTLOOK_SIGNATURE_ALPROSE",
+        //    Company = "Alprose",
+        //    SigSource = Path.Combine(AppContext.BaseDirectory, "Templates", "Alprose"),
+        //    BaseLocal = Path.Combine(AppContext.BaseDirectory, "Output", "Alprose"),
+        //    DefaultPhone = "+41 91 611 88 88",
+        //},
+        //new SignatureParams
+        //{
+        //    GroupName = "SG_CASLANO_DESKTOP_OUTLOOK_SIGNATURE_BARONIE_SWITZERLAND",
+        //    Company = "Baronie Switzerland",
+        //    SigSource = Path.Combine(AppContext.BaseDirectory, "Templates", "Switzerland"),
+        //    BaseLocal = Path.Combine(AppContext.BaseDirectory, "Output", "Switzerland"),
+        //    DefaultPhone = "+41 91 611 88 88",
+        //},
+        //new SignatureParams
+        //{
+        //    GroupName = "SG_Norderstedt_DESKTOP_OUTLOOK_SIGNATURE_DEFAULT",
+        //    Company = "Baronie Norderstedt",
+        //    SigSource = Path.Combine(AppContext.BaseDirectory, "Templates", "Norderstedt"),
+        //    BaseLocal = Path.Combine(AppContext.BaseDirectory, "Output", "Norderstedt"),
+        //    DefaultPhone = "+49 40 52 60 20"
+        //},
+        //new SignatureParams
+        //{
+        //    GroupName = "SG_Eupen_DESKTOP_OUTLOOK_SIGNATURE_DEFAULT",
+        //    Company = "Baronie Eupen",
+        //    SigSource = Path.Combine(AppContext.BaseDirectory, "Templates", "Eupen"),
+        //    BaseLocal = Path.Combine(AppContext.BaseDirectory, "Output", "Eupen"),
+        //    DefaultPhone = "+32 50 45 00 30"
+        //},
+        //new SignatureParams
+        //{
+        //    GroupName = "SG_Saalfeld_DESKTOP_OUTLOOK_SIGNATURE_DEFAULT",
+        //    Company = "Baronie Saalfeld",
+        //    SigSource = Path.Combine(AppContext.BaseDirectory, "Templates", "Saalfeld"),
+        //    BaseLocal = Path.Combine(AppContext.BaseDirectory, "Output", "Saalfeld"),
+        //    DefaultPhone = "+49 40 526020"
+        //},
+        //new SignatureParams
+        //{
+        //    GroupName = "SG_Berlin_DESKTOP_OUTLOOK_SIGNATURE_DEFAULT",
+        //    Company = "Baronie Berlin",
+        //    SigSource = Path.Combine(AppContext.BaseDirectory, "Templates", "Berlin"),
+        //    BaseLocal = Path.Combine(AppContext.BaseDirectory, "Output", "Berlin"),
+        //    DefaultPhone = "+49 40 526020"
+        //},
     ];
 }
 
